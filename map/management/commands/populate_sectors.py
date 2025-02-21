ba2bf759fcdfba97f9185d10d093c18fd80dc805
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-02-21 16:36:04 +0700

    draft 2 ugly html

--- map/management/commands/populate_sectors.py
+++ map/management/commands/populate_sectors.py
@@ -6,19 +6,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         river_basins = [
-            {"name": "แม่น้ำปิง (Ping River)", "latitude": 18.8000, "longitude": 99.0000},
-            {"name": "แม่น้ำเจ้าพระยา (Chao Phraya River)", "latitude": 15.2000, "longitude": 100.5000},
-            {"name": "แม่น้ำโขง (Mekong River)", "latitude": 17.9500, "longitude": 102.6000},
-            {"name": "แม่น้ำยม (Yom River)", "latitude": 17.2500, "longitude": 100.0000},
-            {"name": "แม่น้ำน่าน (Nan River)", "latitude": 18.1000, "longitude": 100.8000},
-            {"name": "แม่น้ำแม่กลอง (Mae Klong River)", "latitude": 13.9000, "longitude": 99.9500},
-            {"name": "แม่น้ำมูล (Mun River)", "latitude": 15.0000, "longitude": 103.2000}
+            {"name": "แม่น้ำปิง (Ping River)", "latitude": 18.8000, "longitude": 99.0000, "image":"sector/image1.png"},
+            
         ]
 
         for basin in river_basins:
             sector, created = Sector.objects.get_or_create(
                 name=basin["name"],
-                defaults={"latitude": basin["latitude"], "longitude": basin["longitude"]}
+                defaults={"latitude": basin["latitude"], "longitude": basin["longitude"], "image": basin["image"]}
             )
 
             if created:
